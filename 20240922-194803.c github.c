8d4f857a7d3b0f6e0ba2c58246a8a688c52413e0
RAJASABARI-BLOOM <rajasabari.ksrct@gmail.com>
2024-09-22 19:48:38 +0530

    Add files via upload

---
+++ 20240922-194803.c github.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float a;
+clrscr();
+printf("enter the no");
+scanf("%f",&a);
+printf("\n square root is %f",sqrt(a));
+getch();
+}
